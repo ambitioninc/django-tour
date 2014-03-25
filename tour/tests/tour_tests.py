9110acf5e5e3b7692e98bacf8fd1a7eae88638cb
Wes Okes <wes.okes@gmail.com>
2014-03-25 16:39:17 -0400

    pep8

--- tour/tests/tour_tests.py
+++ tour/tests/tour_tests.py
@@ -3,7 +3,8 @@ from django.test import TestCase
 from tour.exceptions import MissingStepClass, MissingTourClass
 
 from tour.models import Tour, Step, TourStatus
-from tour.tests.mocks import MockTour, MockStep1, MockStep2, MockTour2, MockStep3, MockStep4, MockTour3, MockTour4, CompleteTour, NoUrlStep
+from tour.tests.mocks import (
+    MockTour, MockStep1, MockStep2, MockTour2, MockStep3, MockStep4, MockTour3, MockTour4, CompleteTour)
 from tour.tours import BaseStep
 
 
