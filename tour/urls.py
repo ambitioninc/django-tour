6a9458b52a602b77b8af46e621b3fd4eed0349da
Wes Okes <wes.okes@gmail.com>
2014-09-30 13:47:26 -0400

    * added tests

--- tour/urls.py
+++ tour/urls.py
@@ -4,9 +4,5 @@ from tour import api
 
 urlpatterns = patterns(
     '',
-    url(
-        r'^api/tour/$',
-        api.TourApiView.as_view(),
-        name='tour.tour_api'
-    ),
+    url(r'^api/tour/$', api.TourApiView.as_view(), name='tour.tour_api'),
 )
